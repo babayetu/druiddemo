0621eb0c9b7afb7a44f10c04dd038090fc4af6ce
jinghome <jjliu@DESKTOP-DFHTRI8.(none)>
2018-04-02 00:10:51 +0800

    first commit

---
+++ src/main/java/com/babayetu/druiddemo/DruiddemoApplication.java
@@ -0,0 +1,12 @@
+package com.babayetu.druiddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DruiddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DruiddemoApplication.class, args);
+	}
+}
